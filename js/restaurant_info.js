b5f5ed7c8d1a758c362afa6ebd5f6a323999163b
et41 <erenturan41@gmail.com>
2018-04-04 12:23:14 +0300

    fix: Change in breadcrumb.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -159,7 +159,7 @@ createReviewHTML = (review) => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
+  const breadcrumb = document.getElementById('breadcrumb-ol');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
